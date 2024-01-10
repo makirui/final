51732b7c1baea68355eae8d05f8c9abc354a9cbb
makirui <148730277+makirui@users.noreply.github.com>
2024-01-10 10:58:41 +0900

    php作成

---
+++ src/menu.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https:/cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+    <title>メニュー</title>
+</head>
+<body>
+    <h1>カフェ一覧メニュー</h1>
